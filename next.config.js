ffccca6a344529c9b08ea05c76850c08aa2950a4
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-10-28 19:26:23 +0600

    initiate

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    newNextLinkBehavior: false,
-  },
-  images: {
-    domains: [
-      'i.ibb.co',
-      'restauranttechnologynetwork.com',
-      'www.caasco.com',
-      'dev-search.foodclub.uk',
-      'newdevmain.foodclub.uk',
-      'devfoodclubuk.s3.eu-west-2.amazonaws.com',
-      'portal.foodclubuk.com',
-      'productionfc.s3.eu-west-2.amazonaws.com',
-    ],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- src/Components/Others/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className='flex justify-center items-center my-12 mx-auto'>
-      <div className='animate-spin rounded-full h-24 w-24 sm:h-32 sm:w-32 border-t-4 border-b-4 border-red-500'></div>
-    </div>
-  );
-};
-
-export default Loader;
